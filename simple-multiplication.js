6628b8d37fde1897a123bebad2cba91eecea1c3d
Javier Mateus <151576856+Jafemago@users.noreply.github.com>
2023-11-22 17:49:04 -0400

    Create simple-multiplication.js
    Kata

---
+++ simple-multiplication.js
@@ -0,0 +1,9 @@
+// simple multiplication: by 8 if is an even number or 9 if not
+
+function simpleMultiplication(number) {
+    if (number %2==0){
+      return (number*8)
+    }  else {
+      return (number*9)
+    }
+}
